7b1f021083224b16159b40b665d57ee9f6593bc8
unknown <hello@santy.in>
2023-03-05 12:51:36 +0530

    intial setup and todo list

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+
+<template>
+  <div>
+    <div>
+      <a href='#/todo'>ToDo</a>
+    </div>
+  </div>
+  <component :is = currentView />
+</template>
+
+<script>
+import ToDo from './components/ToDo.vue'
+const routes ={
+  '/todo': ToDo
+}
+  export default{
+    data(){
+      return {
+        currentPath: window.location.hash
+      }
+    },
+    computed:{
+      currentView(){
+        return routes[this.currentPath.slice(1) || '/']
+      }
+    },
+    mounted(){
+      window.addEventListener('hashchange',()=>{
+        this.currentPath = window.location.hash
+      })
+    }
+  }
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ToDo.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <router-view>
+    <ul>
+      <li v-for="item of items" :key='item.id'>
+        <input type="checkbox" v-model="item.completed">
+        {{ item.text }}
+      </li>
+    </ul>
+    <form @submit.prevent = 'addItem'>
+      <input type="text" v-model="newItem" placeholder="Add new Item">
+      <button type="submit">Add</button>
+    </form>
+  </router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      title: 'ToDo List',
+      items : [
+        {id:1, text: 'Learn Vue', completed:false},
+        {id:2, text:'Build An App', completed:true}
+      ],
+      newItem:'',
+    }
+  },
+  methods:{
+    addItem(){
+      if(this.newItem.trim() !== ''){
+        this.items.push({
+          id:Date.now(),
+          text:this.newItem.trim(),
+          completed:false,
+        });
+        this.newItem='';
+      }
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
